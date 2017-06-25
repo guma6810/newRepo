fe1fa2bed2169db78bef099305f7ac99bebfeadf
komieun <komieun@220.149.235.55>
2017-06-25 15:06:25 +0900

    1. InputFunc 클래스 추

---
+++ NewProject/src/InputFunc.java
@@ -0,0 +1,6 @@
+
+public class InputFunc {
+	public void inputFunction() {
+		
+	}
+}
--- NewProject/src/Main.java
+++ NewProject/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("코드가 수정되었습니다.");
-		
+		InputFunc ifunc = new InputFunc();
 	}
 	
 	public String newFunction() {
