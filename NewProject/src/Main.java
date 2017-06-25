b8656c57da48c6a4627a8ead48299c4157db7fd0
komieun <komieun@220.149.235.55>
2017-06-25 15:24:22 +0900

    1. 삭제

--- NewProject/src/Main.java
+++ NewProject/src/Main.java
@@ -4,7 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("코드가 수정되었습니다.");
-		InputFunc ifunc = new InputFunc();
 	}
 	
 	public void newFunction() {
