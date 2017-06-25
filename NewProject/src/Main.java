e3fab525e9c772efebc7aecad34b8e5ab0ee5e3b
komieun <komieun@220.149.235.55>
2017-06-25 15:39:11 +0900

    1. 강제 적용

--- NewProject/src/Main.java
+++ NewProject/src/Main.java
@@ -4,6 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("코드가 수정되었습니다.");
+		
+		System.out.println();
 	}
 	
 	public void newFunction() {
